1cde053106c79e2430cf2c4be8a0d685ef3b0529
TGVM <tgvm96@gmail.com>
2024-03-14 10:27:36 -0300

    options menu

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
 
     public static SoundManager Instance { get; private set; }
 
     [SerializeField] private AudioClipsRefsSO audioClipsRefsSO;
 
+    private float volume = 1f;
+
+
     private void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
 
 
@@ -68,14 +74,28 @@ public class SoundManager : MonoBehaviour
     
     }
 
-    private void Playsound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void Playsound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume)
     {
         Playsound(audioClipsRefsSO.footsteps, position, volume);
     }
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        if(volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume() { return volume; }
 
 }
