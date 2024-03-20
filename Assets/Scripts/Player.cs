20c944d6eaa2383fdc38525335b01f836921ecd3
TGVM <tgvm96@gmail.com>
2024-03-20 11:18:44 -0300

    client-host sync

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -62,6 +62,10 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
         HandleMovement();
         HandleInteractions();
     }
